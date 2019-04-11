c90a9dd4a9f925eaf45c42e1dbe136a9defd6246
ishita4 <43470474+ishita4@users.noreply.github.com>
2019-04-11 16:14:07 +0530

    Update levelTwo.cpp

--- levelTwo.cpp
+++ levelTwo.cpp
@@ -3,7 +3,8 @@
 #include<graphics.h>
 #include <dos.h>
 int d, shouldMove;
-int sceneOneFlag=1,sceneTwoFlag=1;
+int sceneOneFlag=1,sceneTwoFlag=1,sceneThreeFlag=1,sceneFourFlag=1;
+
 void displayMan(int x,int y,int colour)
 {
     setcolor(colour);
@@ -13,8 +14,6 @@ void displayMan(int x,int y,int colour)
     line(x,y+30,x+20,y+40);    //right hand
     line(x,y+30,x,y+60);    //body
     setcolor(WHITE);
-//line(x+30,y+30,x+30,y-90);  //umbrella
-//pieslice(x+30,y-30,0,180,55);
 }
 
 void drawFlag()
@@ -80,8 +79,55 @@ void drawBrickWall()
 
     setfillstyle(SOLID_FILL,BROWN);
     floodfill(180,240,WHITE);
+}
+
+void drawPipe()
+{
+    outtextxy(355, 290, "I");
+    outtextxy(355, 310, "U");
+    outtextxy(355, 330, "P");
+    outtextxy(355, 350, "A");
+    outtextxy(355, 370, "C");
+
+    line(330,250,330,430);
+    line(380,250,380,430);
+
+    line(320,250,390,250);
+    line(320,220,390,220);
+    line(320,220,320,250);
+    line(390,220,390,250);
+
+    setfillstyle(SOLID_FILL,GREEN);
+    floodfill(350,300,WHITE);
+    floodfill(350,230,WHITE);
+}
+
+void drawStaircase()
+{
+
+    line(200,390,260,390);
+    line(260,350,320,350);
+    line(320,310,380,310);
+    line(380,270,440,270);
+    line(440,230,getmaxx(),230);
+
+    line(200,390,200,430);
+    line(260,350,260,390);
+    line(320,310,320,350);
+    line(380,270,380,310);
+    line(440,230,440,270);
+
+    setfillstyle(INTERLEAVE_FILL,RED);
+    floodfill(500,420,WHITE);
+
+    outtextxy(210, 400, "Practicals");
+    outtextxy(270, 360, "River-Boat Problem");
+    outtextxy(330, 320, "Past Year Questions");
+    outtextxy(390, 280, "HC Verma");
+    outtextxy(450, 240, "Mock Tests");
 
 }
+
 
 void jump(int x, int y,int factor)
 {
@@ -146,8 +192,7 @@ int moveMan(int x, int y)
 
 void levelTwo()
 {
-    //int i,d=0,x=50,y=340,z=50,shouldMove=1;
-    int i,x=50,y=340,z=50;
+    int i,x=50,y=340,z=50,page=0;
 
     while(!kbhit())
     {
@@ -229,7 +274,7 @@ void levelTwo()
 
             }
 
-            //Dropping coin on man's head
+            // Dropping coin on man's head
             int temp=200;
             for(int i=0;i<11;i++)
             {
@@ -254,22 +299,232 @@ void levelTwo()
             floodfill(340,35,WHITE);
             delay(1000);
 
-            while(x<=510)
+            float temp2;
+            while(x<=460)
             {
-                cleardevice();
                 line(0,430,640,430);
-                displayMan(x,345,WHITE);
+
                 drawBrickWall();
+                drawPipe();
+                //Display message
                 outtextxy(280, 30, "Coins            X 10");
                 circle(340,35,10);
                 setfillstyle(SOLID_FILL,YELLOW);
                 floodfill(340,35,WHITE);
+
+                 if(x<=355)
+                 {
+                    circle(355,200,15);
+                    setfillstyle(SOLID_FILL,YELLOW);
+                    floodfill(355,200,WHITE);
+                 }
+                 else{
+                    outtextxy(280, 30, "Coins            X 20");
+                    outtextxy(300, 450, "Tadaaa");
+                    circle(340,35,10);
+                    setfillstyle(SOLID_FILL,YELLOW);
+                    floodfill(340,35,WHITE);
+                 }
+
+                 if(x<250)
+                 {
+                    circle(355,200,15);
+                    setfillstyle(SOLID_FILL,YELLOW);
+                    floodfill(355,200,WHITE);
+                     displayMan(x,345,WHITE);
+                     x=moveMan(x,y);
+                 }
+
+                else
+                {
+                    temp2=(0.0208*x*x)-(14.81*x)+(2744);
+                    displayMan(x,temp2,WHITE);
+                    line(x,temp2+60,x-8,temp2+85);
+                    line(x,temp2+60,x+8,temp2+85);
+
+                    delay(150);
+                    x=x+10;
+                }
+
+            cleardevice();
+            }
+
+            float temp3;
+             while(x<=570)
+            {
+                cleardevice();
+                line(0,430,640,430);
+                outtextxy(500, 440, "Kinematics");
+                drawBrickWall();
+                drawPipe();
+                //Display message
+                outtextxy(280, 30, "Coins            X 20");
+                circle(340,35,10);
+                setfillstyle(SOLID_FILL,YELLOW);
+                floodfill(340,35,WHITE);
+
+                //Coin on top of block
+                if(x<=560)
+                {
+                    circle(560,380,15);
+                    setfillstyle(SOLID_FILL,YELLOW);
+                    floodfill(560,380,WHITE);
+                }
+
+                 if(x<=470)
+                 {
+                    setfillstyle(HATCH_FILL,LIGHTCYAN);
+                    bar(480,400,600,430);
+                     displayMan(x,345,WHITE);
+                     x=moveMan(x,y);
+                 }
+
+                else
+                {
+                    temp3=  (0.0585*x*x)-(60.6*x)+15870;
+                    displayMan(x,temp3,WHITE);
+                    line(x,temp3+60,x-8,temp3+85);
+                    line(x,temp3+60,x+8,temp3+85);
+                    if(x>560)
+                    {
+                        setfillstyle(HATCH_FILL,LIGHTCYAN);
+                        bar(480,temp3+85,600,430);
+                    }
+                    else
+                    {
+                        setfillstyle(HATCH_FILL,LIGHTCYAN);
+                        bar(480,400,600,430);
+                    }
+
+                    delay(200);
+                    x=x+10;
+                }
+
+            }
+
+            while(x<=getmaxx())
+            {
+                cleardevice();
+                line(0,430,640,430);
+                 outtextxy(500, 440, "Kinematics");
+
+                drawBrickWall();
+                drawPipe();
+                setfillstyle(HATCH_FILL,LIGHTCYAN);
+                bar(480,425,600,430);
+                //Display message
+                outtextxy(280, 30, "Coins            X 30");
+                outtextxy(300, 450, "Yipee");
+                circle(340,35,10);
+                setfillstyle(SOLID_FILL,YELLOW);
+                floodfill(340,35,WHITE);
+                displayMan(x,345,WHITE);
                 x=moveMan(x,y);
+
+            }
+            sceneTwoFlag=0;
+        }
+
+        /********************************** SCENE THREE ****************************************************/
+        d=0,x=50,y=340,z=50,shouldMove=1;
+
+        if(sceneThreeFlag)
+        {
+            while(x<150)
+            {
+                cleardevice();
+                line(0,430,640,430);
+                drawStaircase();
+                outtextxy(280, 30, "Coins            X 30");
+                circle(340,35,10);
+                setfillstyle(SOLID_FILL,YELLOW);
+                floodfill(340,35,WHITE);
+                displayMan(x,345,WHITE);
+                x=moveMan(x,y);
+            }
+            x=230;
+            int temp4=305;
+            while(x<=480)
+            {
+                cleardevice();
+                line(0,430,640,430);
+                drawStaircase();
+
+                outtextxy(280, 30, "Coins            X 30");
+                circle(340,35,10);
+                setfillstyle(SOLID_FILL,YELLOW);
+                floodfill(340,35,WHITE);
+
+                displayMan(x,temp4,WHITE);
+                line(x,temp4+60,x-8,temp4+85);
+                line(x,temp4+60,x+8,temp4+85);
+
+                x+=60;
+                temp4-=40;
+                delay(700);
+            }
+            //Drawing end flag
+            outtextxy(500,80,"Success");
+            line(500,230,500,100);
+            line(505,230,505,100);
+            line(500,100,505,100);
+            setfillstyle(SOLID_FILL,BROWN);
+            floodfill(502,101,WHITE);
+
+            line(505,100,575,125);
+            line(575,125,505,150);
+            setfillstyle(SOLID_FILL,RED);
+            floodfill(510,110,WHITE);
+            delay(400);
+
+            x=470;
+            while(x<=getmaxx())
+            {
+
+                line(0,430,640,430);
+                drawStaircase();
+
+                outtextxy(280, 30, "Coins            X 100");
+                circle(340,35,10);
+                setfillstyle(SOLID_FILL,YELLOW);
+                floodfill(340,35,WHITE);
+
+                outtextxy(500,80,"Success");
+                outtextxy(510,15,"Promoted to");
+                outtextxy(510,35,"Standard 12");
+
+                //Arrow
+                line(590,30,610,30);
+                line(590,35,610,35);
+                line(590,30,590,35);
+
+                line(610,30,610,25);
+                line(610,35,610,40);
+
+                line(610,25,630,32);
+                line(610,40,630,32);
+
+                setfillstyle(SOLID_FILL,YELLOW);
+                floodfill(600,32,WHITE);
+
+                //Drawing end flag
+                line(500,230,500,100);
+                line(505,230,505,100);
+                line(500,100,505,100);
+                setfillstyle(SOLID_FILL,BROWN);
+                floodfill(502,101,WHITE);
+
+                line(505,100,575,125);
+                line(575,125,505,150);
+                setfillstyle(SOLID_FILL,RED);
+                floodfill(510,110,WHITE);
+                displayMan(x,145,WHITE);
+                x=moveMan(x,140);
+                cleardevice();
 
             }
         }
     }
-
 }
 
 int main()
